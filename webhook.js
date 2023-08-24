944ba02a1edfe08dc5631ba9f73bcc339ea3afd3
MSWS <imodmaker@gmail.com>
2023-08-23 23:09:25 -0700

    Merge

--- webhook.js
+++
@@ -1,9 +0,0 @@
-import XMLHttpRequest from 'xhr2';
-
-export function sendMessage(url, params) {
-  const request = new XMLHttpRequest();
-  request.open('POST', url, true);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify(params));
-  console.log(request.responseText);
-}
